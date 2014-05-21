2b9a1f2864ad2d7820761235c43978f3b4680b6d
mlefree <mathieu.leprevost@apps4pro.com>
2014-05-21 21:16:24 +0200

    MLE: add build

--- build_mobile.sh
+++ build_mobile.sh
@@ -5,15 +5,21 @@ echo ""
 echo "[--- C4P : Build mobile ---]"
 echo ""
 
-sudo npm update -g cordova
+#sudo npm update -g cordova
 rm -rf build
 
 cordova create build com.apps4pro.c4p c4p
 
 cp -r www build/.
+cp build/www/config.xml build/config.xml
+
+oldstring='src=\"mobile_res\/'
+newstring='src=\"..\/www\/mobile_res\/'
+sed -i.bak "s#$oldstring#$newstring#g" build/config.xml
+
 
 cd build
-cordova platform add ios
+#cordova platform add ios
 cordova platform add android
 
 
@@ -47,7 +53,8 @@ cordova plugin add https://github.com/mhweiner/CordovaiOSKeyboardPlugin.git
 # Sed AndroidManifest
 
 oldstring='<\/intent-filter>'
-newstring='<\/intent-filter ><intent-filter><action android:name="android.intent.action.VIEW"><\/action><category android:name="android.intent.category.DEFAULT"><\/category><category android:name="android.intent.category.BROWSABLE"><\/category><data android:scheme="content" android:mimeType="*\/*"><\/data><data android:scheme="file" android:mimeType="*\/*"\/><data android:host="www.youtube.com" android:scheme="http"><\/data><\/intent-filter>'
+newstring='<\/intent-filter ><intent-filter><action android:name="android.intent.action.VIEW"\/><action android:name="android.intent.action.EDIT"\/><category android:name="android.intent.category.DEFAULT"\/><data android:scheme="file" android:mimeType="*\/*"\/><data android:scheme="http" android:mimeType="*\/*"\/><data android:scheme="content" android:mimeType="*\/*"\/><\/intent-filter>'
+#newstring='<\/intent-filter ><intent-filter><action android:name="android.intent.action.VIEW"><\/action><category android:name="android.intent.category.DEFAULT"><\/category><category android:name="android.intent.category.BROWSABLE"><\/category><data android:scheme="content" android:mimeType="*\/*"><\/data><data android:scheme="file" android:mimeType="*\/*"\/><data android:host="www.youtube.com" android:scheme="http"><\/data><\/intent-filter>'
 sed -i.bak "s#$oldstring#$newstring#g" platforms/android/AndroidManifest.xml
 
 
@@ -56,9 +63,9 @@ cordova build android
 cd platforms/android
 ant release
 
-jarsigner -keystore ../../../../../c4p/c4p_html_ang/mobile_res/android_key/apps4pro-key.keystore -storepass apps4pro -digestalg SHA1 -sigalg MD5withRSA bin/c4p-release-unsigned.apk mykey
-cp bin/c4p-release-unsigned.apk ../../c4p.apk
-zipalign -f 4 ../../c4p.apk ../../c4p-aligned.apk
+jarsigner -keystore ../../../../../c4p/c4p_html_ang/mobile_res/android_key/apps4pro-key.keystore -storepass apps4pro -digestalg SHA1 -sigalg MD5withRSA bin/CRMMeetingPad-release-unsigned.apk mykey
+cp bin/CRMMeetingPad-release-unsigned.apk ../../CRMMeetingPad.apk
+zipalign -f 4 ../../CRMMeetingPad.apk ../../CRMMeetingPad-aligned.apk
 
 
 cd ../../..
